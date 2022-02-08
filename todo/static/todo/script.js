d4d92d1ad250d79aac11b6ab69506b0fdcbc5215
hinkyma <osocki@gmail.com>
2022-02-08 22:45:09 +0300

    Finished todowoo-project!

---
+++ todo/static/todo/script.js
@@ -0,0 +1,36 @@
+const body = document.querySelector("body"),
+      sidebar = body.querySelector(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+
+      let getMode = localStorage.getItem("mode");
+        if(getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+        }
+
+
+toggle.addEventListener("click", () =>{
+sidebar.classList.toggle("close");
+});
+
+modeSwitch.addEventListener("click", () =>{
+    body.classList.toggle("dark");
+
+    if(!body.classList.contains("dark")){
+        localStorage.setItem("mode", "light-mode");
+    }else{
+        localStorage.setItem("mode", "dark-mode");
+    }
+
+
+
+if(body.classList.contains("dark")){
+    modeText.innerText = "Light Mode"
+}else{
+    modeText.innerText = "Dark Mode"
+}
+});
+
+
+
